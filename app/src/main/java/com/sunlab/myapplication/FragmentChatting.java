0020a82a558150780502c646cf378ae66a638d9e
tmsgod2 <tmsgod2@naver.com>
2020-03-31 15:59:40 +0900

    ddd

--- app/src/main/java/com/sunlab/myapplication/FragmentChatting.java
+++ app/src/main/java/com/sunlab/myapplication/FragmentChatting.java
@@ -10,9 +10,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FragmentChatting extends Fragment {
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chatting,container,false);
+
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_chatting,container,false);
+
+
+        return rootView;
+
     }
 }
